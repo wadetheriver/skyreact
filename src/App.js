3b1ad764c4b8da11bdab9a56bb479baa632536a4
Wade A Spane <wadetheriver@gmail.com>
2021-11-11 08:00:45 -0800

    map appointment list of json data in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import { GiMagicBroom } from "react-icons/gi"
+import { BiTrash } from "react-icons/bi"
 import Search from "./components/Search"
 import AddAppointment from "./components/AddAppointment"
-// import './App.css';
+import appointmentList from "./data.json"
+
+
 
 function App() {
+
+  console.log (appointmentList);
   return (
     <div className="App container mx-auto mt-3 font-thin ">
       <h1 className = "text-5xl">
         <GiMagicBroom className="inline-block text-red-400 align-top"/>
-         Hello</h1>
+         Bitch With A Broom.</h1>
+
          <AddAppointment />
          <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {appointmentList
+          .map(appointment => (
+            <li className="px-3 py-3 flex items-start">
+              <button type="button"
+                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <BiTrash /></button>
+              <div className="flex-grow">
+                <div className="flex items-center">
+                  <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
+                  <span className="flex-grow text-right">{appointment.aptDate}</span>
+                </div>
+                <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
+                <div className="leading-tight">{appointment.aptNotes}</div>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
 
 
     </div>
